327ebaffe476c80da6e753fdad140cf33d25947f
Vova <kandrin2002@mail.ru>
2022-12-25 17:24:32 +0100

    сервисы

---
+++ lib/services/errors/input_errors.dart
@@ -0,0 +1,7 @@
+class InputErrors {
+  static const invalidFormat = 'Неверный формат';
+  static const minLength = 'Минимальная длина - 3 символа';
+  static const maxLength = 'Превышен лимит символов';
+  static const empty = 'Обязательно для заполнения';
+  static const collectionAlreadyExists = 'Название коллекции уже занято';
+}
---
+++ lib/services/extensions/string_extension.dart
@@ -0,0 +1,4 @@
+extension StringExtension on String {
+  bool isMaxLength() => length > 24;
+  bool isMinLength() => length < 3;
+}
