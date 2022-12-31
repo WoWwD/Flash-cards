f5789b8a8a96fb702ce640a34ff3fd448e1d9875
Vova <kandrin2002@mail.ru>
2022-12-31 15:27:44 +0100

    доделал обучение

--- lib/presentation/provider/learning_provider_model.dart
+++ lib/presentation/provider/learning_provider_model.dart
@@ -5,24 +5,30 @@ import '../../data/model/flash_card_model.dart';
 class LearningProviderModel extends ChangeNotifier {
   final Random _rnd = Random();
   List<FlashCard> _listCards = [];
-  int _numberCard = 0;
-
+  late int _numberCard;
 
   int get numberCard => _numberCard;
   List<FlashCard> get listCards => _listCards;
 
   void getListCards(List<FlashCard> listCards) {
-    _listCards = listCards;
+    _numberCard = 0;
+    _listCards = _mixCards(listCards);
     notifyListeners();
   }
 
   void nextCard() {
-    _numberCard = _rnd.nextInt(_listCards.length);
+    _numberCard += 1;
     notifyListeners();
   }
 
-  void learned() {
-    _listCards.removeAt(_numberCard);
-    notifyListeners();
+  List<FlashCard> _mixCards(List<FlashCard> listCards) {
+    for (int i = listCards.length - 1; i >= 1; i--)
+    {
+      int j = _rnd.nextInt(i + 1);
+      var temp = listCards[j];
+      listCards[j] = listCards[i];
+      listCards[i] = temp;
+    }
+    return listCards;
   }
 }
--- lib/presentation/ui/screens/learning_screen.dart
+++ lib/presentation/ui/screens/learning_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flash_cards/data/model/dictionary_model.dart';
 import 'package:flash_cards/presentation/provider/learning_provider_model.dart';
+import 'package:flash_cards/presentation/ui/widgets/primary_alert_dialog_widget.dart';
 import 'package:flash_cards/presentation/ui/widgets/primary_scaffold_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_card/flash_card.dart';
@@ -22,38 +23,38 @@ class LearningScreen extends StatelessWidget {
         titleText: 'Обучение',
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 48),
-          child: Column(
-            children: [
-              Expanded(
-                child: Consumer<LearningProviderModel>(
-                  builder: (context, model, child) {
-                    return FlashCard(
+          child: Consumer<LearningProviderModel>(
+            builder: (context, model, child) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: FlashCard(
                       duration: const Duration(milliseconds: 400),
                       height: 400,
                       width: 300,
                       frontWidget: _getWord(model.listCards[model.numberCard].word),
                       backWidget: _getWord(model.listCards[model.numberCard].translate)
-                    );
-                  },
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    iconSize: 46,
-                    icon: const Icon(Icons.done, color: Colors.green),
-                    onPressed: () => context.read<LearningProviderModel>().learned(),
+                    ),
                   ),
-                  const SizedBox(width: 50),
                   IconButton(
+                    splashRadius: 36,
                     iconSize: 46,
                     icon: const Icon(Icons.next_plan),
-                    onPressed: () => context.read<LearningProviderModel>().nextCard(),
+                    onPressed: () {
+                      if (model.numberCard == model.listCards.length - 1) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => _cardsAreOver(context, model, dictionaryModel)
+                        );
+                      }
+                      else {
+                        model.nextCard();
+                      }
+                    },
                   )
                 ],
-              )
-            ],
+              );
+            },
           ),
         )
       ),
@@ -63,6 +64,28 @@ class LearningScreen extends StatelessWidget {
   Widget _getWord(String text) {
     return Center(
       child: Text(text, style: const TextStyle(fontSize: 22)),
+    );
+  }
+
+  Widget _cardsAreOver(BuildContext context, LearningProviderModel model, Dictionary dictionaryModel) {
+    return PrimaryAlertDialog(
+      textTitle: 'Пройти заново?',
+      actions: [
+        TextButton(
+          onPressed: () {
+            model.getListCards(dictionaryModel.listCards);
+            Navigator.pop(context);
+          },
+          child: const Text('Да')
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+            Navigator.pop(context);
+          },
+          child: const Text('Нет')
+        ),
+      ]
     );
   }
 }
